9eb0ff01d6d86f10b2a1f3db2479433295095ac1
Mengchenxi <290815385@qq.com>
2022-08-10 18:34:59 +0800

    完成了数据持久化

--- src/views/Search/components/SearchHistory.vue
+++ src/views/Search/components/SearchHistory.vue
@@ -1,10 +1,66 @@
 <template>
-  <div>搜索历史</div>
+  <div classs="SearchHistory-container">
+    <!-- 1.标题 -->
+    <van-cell title="搜索历史">
+      <div v-if="isDeleteShow">
+        <span @click="$emit('clearAll')">全部删除</span>
+        &nbsp; &nbsp;
+        <span @click="isDeleteShow = false">完成</span>
+      </div>
+      <van-icon v-else name="delete-o" @click="isDeleteShow = true" />
+    </van-cell>
+
+    <!-- 2.历史记录 -->
+    <van-cell
+      :title="item"
+      v-for="(item, index) in history"
+      :key="index"
+      @click="onSearchItemClick(item, index)"
+    >
+      <!-- 使用 right-icon 插槽来自定义右侧图标 -->
+      <template #right-icon>
+        <van-icon v-show="isDeleteShow" name="close" />
+      </template>
+    </van-cell>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'SearchHistory'
+  name: 'SearchHistory',
+  data () {
+    return {
+      isDeleteShow: false // 控制图标的显示和隐藏
+    }
+  },
+  // prop 数据
+  // 如果是普通数据(数字/字符串/布尔值) 绝对不能修改 即便修改也不能传递给父组件
+  // 如果是引用类型数据(数组/对象)
+  // 可以修改 例如 [].push()
+  // 但是不能重新赋值 xxx=[]
+  props: {
+    keywords: {
+      type: String,
+      required: true
+    },
+    history: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    onSearchItemClick (item, index) {
+      // 删除状态,删除该项搜索历史
+      if (this.isDeleteShow) {
+        this.$parent.history.splice(index, 1)
+      } else {
+        // 非删除状态切换显示搜索结果
+        // this.$parent.keywords = item
+        // this.$parent.isShowSearchResult = true
+        this.$emit('search', item)
+      }
+    }
+  }
 }
 </script>
 
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -20,7 +20,13 @@
     <search-suggestion></search-suggestion> -->
     <!--动态组件 -->
     <!-- is指定动态组件名字,里面的值是字符串 并且值应该从注册的components里面取-->
-    <components :is="componentName" :keywords="keywords"></components>
+    <components
+      :is="componentName"
+      :keywords="keywords"
+      :history="history"
+      @clearAll="history = []"
+      @search="onSearch"
+    ></components>
   </div>
 </template>
 
@@ -28,18 +34,28 @@
 import SearchHistory from './components/SearchHistory'
 import SearchResult from './components/SearchResult'
 import SearchSuggestion from './components/SearchSuggestion'
+import storage from '@/utils/storage'
 export default {
   name: 'Search',
   data () {
     return {
       keywords: '',
       //   用于记录用户是否搜索
-      isShowSearchResult: false
+      isShowSearchResult: false,
+      // 搜索记录
+      history: storage.get('TOUTIAO_SEARCH_HISTORY') || []
     }
   },
   methods: {
-    onSearch () {
-      console.log('正在搜索')
+    onSearch (val) {
+      // console.log('正在搜索')
+      const index = this.history.indexOf(val)
+      // 说明历史记录里有这个值
+      if (index !== -1) {
+        this.history.splice(index, 1)
+      }
+      this.history.unshift(val)
+      // 展示搜索结果
       this.isShowSearchResult = true
     },
     onSearchFocus () {
@@ -66,6 +82,12 @@ export default {
       }
       // 既不显示搜索历史 也不是显示搜索结果 就渲染搜索建议
       return 'SearchSuggestion'
+    }
+  },
+  watch: {
+    history (value) {
+      storage.set('TOUTIAO_SEARCH_HISTORY', value)
+      // localStorage.setItem(('TOUTIAO_SEARCH_HISTORY', JSON.stringify(value)))
     }
   }
 }
